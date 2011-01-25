34b07f03ba972c76d24197ec845819063b00de61
Michael F <mefurth@miners.utep.edu>
2011-01-24 18:43:24 -0700

    Merge with master

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -126,9 +126,7 @@ bool ChatHandler::HandleServerInfoCommand(char* /*args*/)
     PSendSysMessage(LANG_USING_EVENT_AI,sWorld.GetCreatureEventAIVersion());
     PSendSysMessage(LANG_CONNECTED_USERS, activeClientsNum, maxActiveClientsNum, queuedClientsNum, maxQueuedClientsNum);
     PSendSysMessage(LANG_UPTIME, str.c_str());
-    SendSysMessage("Welcome to The Infinity Project");
-    SendSysMessage("GIT: http://github.com/FallenAngelX");
-	SendSysMessage("Please feel free to post bugs and Fixes on GiT ");
+    SendSysMessage("Welcome to RaidenWoW");
     SendSysMessage("A heavily modified MaNGOS core & SD2 System");
 
     return true;

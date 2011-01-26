c586666b113db47aff2dd7ed63eab9ce13bdec02
GMHAZE <iceis2@hotmail.com>
2011-01-26 03:56:23 -0500

    {XXX} added visual effect for flying creatures ( needs more testing and maybe more research )

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -254,6 +254,11 @@ void Object::BuildMovementUpdate(ByteBuffer * data, uint16 updateFlags) const
                 {
                     // (ok) most seem to have this
                     unit->m_movementInfo.AddMovementFlag(MOVEFLAG_LEVITATING);
+					
+                    // Add flying effect. This should be in db, having trouble with pets
+					// needs more testing
+					//if(!((Creature*)unit)->HasSplineFlag(SPLINEFLAG_UNKNOWN7))
+                    //    ((Creature*)unit)->AddSplineFlag(SPLINEFLAG_UNKNOWN7);
 
                     if (!((Creature*)unit)->hasUnitState(UNIT_STAT_MOVING))
                     {

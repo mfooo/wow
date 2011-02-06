2d88d8fe59a264f54380de7349bba42652afd9ab
GMHAZE <iceis2@hotmail.com>
2011-02-06 00:35:05 -0500

    possible bug fix needs implented and tested

--- src/game/CreatureAI.cpp
+++ src/game/CreatureAI.cpp
@@ -27,6 +27,11 @@ CreatureAI::~CreatureAI()
 
 void CreatureAI::AttackedBy( Unit* attacker )
 {
+    /// TEST THIS OUT after fixing
+    // vehicle dont have threat list, so this is unnecessary, because it calls move chase
+    //if(m_creature->IsVehicle())
+   //     return;
+		
     if(!m_creature->getVictim())
         AttackStart(attacker);
 }

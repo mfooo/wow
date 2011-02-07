94349b3a820f301f56f942313d13f26ef3bca66c
GMHAZE <iceis2@hotmail.com>
2011-02-06 23:24:16 -0500

    allow use of GetMangosString in scripts by janu

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -7845,6 +7845,11 @@ const char *ObjectMgr::GetMangosString(int32 entry, int locale_idx) const
     return "<error>";
 }
 
+const char * GetMangosString(int32 entry, int locale_idx) 
+{ 
+  return sObjectMgr.GetMangosString(entry, locale_idx); 
+}
+
 void ObjectMgr::LoadSpellDisabledEntrys()
 {
     m_spell_disabled.clear();                                // need for reload case
